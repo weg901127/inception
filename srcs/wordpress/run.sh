64edcbc5b00ca05b6883354814b14c6bdb5a0daa
gilee <gilee@student.42seoul.kr>
2022-05-31 18:35:29 +0900

    finish

--- srcs/wordpress/run.sh
+++ srcs/wordpress/run.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 sed -i 's/listen = 127.0.0.1:9000/listen = 0.0.0.0:9000/g' /etc/php8/php-fpm.d/www.conf
+sed -i 's/;clear_env = no/clear_env = no/g' /etc/php8/php-fpm.d/www.conf
+
 exec php-fpm8 -F
